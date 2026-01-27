35fb8bb14b700449d2fc8640eec53e571a5b839d
Im-dexter <sourabhsingh08011@gmail.com>
2026-01-27 12:44:06 +0530

    small bug fix

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -18,7 +18,7 @@ const renderText = (text, className, baseWeight = 400) => {
 };
 
 const setupTextHover=(container,type)=>{
-    if(!container) return;
+    if(!container) return ()=>{};
 
     const letters=container.querySelectorAll("span");
     const {min, max, default:base}=FONT_WEIGHTS[type];
@@ -78,7 +78,7 @@ const Welcome = () => {
 
     <div className="small-screen">
         <p>
-            This portfolio is designed for laptops and tablets only.
+            This portfolio is designed for laptops and tablet only.
         </p>
     </div>
     </section>
