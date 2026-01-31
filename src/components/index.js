4a9f451ac7b8e2834144e45a89464e303f564237
Im-dexter <sourabhsingh08011@gmail.com>
2026-02-01 01:39:48 +0530

    implement dock presentation

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 import Navbar from "./Navbar";
 import Welcome from "./Welcome";
+import Dock from "./Dock";
 
-export { Navbar, Welcome };
+
+export { default as Navbar } from "./Navbar.jsx";
+export { default as Welcome } from "./Welcome.jsx";
+export { default as Dock } from "./Dock.jsx";
