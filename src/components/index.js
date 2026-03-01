fbf82b69c40862fb301461cc35e8410705631ea8
Im-dexter <sourabhsingh08011@gmail.com>
2026-03-02 03:16:08 +0530

    implemented a higher order component for managing window state and added the first window - terminal

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import Welcome from "./Welcome";
 import Dock from "./Dock";
+import WindowControl from "#components/WindowControls.jsx";
 
 
-export { Navbar, Welcome, Dock };
+export { Navbar, Welcome, Dock, WindowControl };
